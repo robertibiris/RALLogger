f39c67d0e0db9b630abcdea5bedde32062a637fb
Roberto Arreaza <rca.cire@gmail.com>
2024-09-21 11:40:52 +0200

    added public interfaces to use from external projects

--- Sources/RALLogger/RALLogger.swift
+++ Sources/RALLogger/RALLogger.swift
@@ -7,19 +7,19 @@
 import Foundation
 
 public class Logger {
-    static let shared = Logger(topLevel: .info)
+    public static let shared = Logger(topLevel: .info)
     ///When `true`, logs will also appear in the physical device's logs.
-    var logsIntoConsole: Bool = false
+    public var logsIntoConsole: Bool = false
     
-    var topLevel: LogLevel
+    public var topLevel: LogLevel
     
     /// initializes a RALLogger with a maximum loggin level
     /// - Parameter topLevel: the highest level of events to log. (the higher => the more events will be logged)
-    init(topLevel: LogLevel = .info) {
+    public init(topLevel: LogLevel = .info) {
         self.topLevel = topLevel
     }
     
-    func log(_ message: String, level: LogLevel = .info, function: String = #function, file: String = #file, line: Int = #line) {
+    public func log(_ message: String, level: LogLevel = .info, function: String = #function, file: String = #file, line: Int = #line) {
         
         if level.rawValue <= self.topLevel.rawValue {
             let fileName = file.components(separatedBy: "/").last ?? ""
@@ -35,14 +35,14 @@ public class Logger {
     }
     
     /// Same as log(), but fails right afterwards (using AssertionFailure). This SHOULD NOT impact release builds.
-    func logAndFail(_ message: String, level: LogLevel = .info, function: String = #function, file: String = #file, line: Int = #line) {
+    public func logAndFail(_ message: String, level: LogLevel = .info, function: String = #function, file: String = #file, line: Int = #line) {
         
         self.log(message, level: level, function: function, file: file, line: line)
         assertionFailure(message)
     }
     
     /// assert a given condition and logAndFail if it's not met. This SHOULD NOT impact release builds.
-    func assert(_ condition: Bool, failureMessage message: String, level: LogLevel = .info, function: String = #function, file: String = #file, line: Int = #line) {
+    public func assert(_ condition: Bool, failureMessage message: String, level: LogLevel = .info, function: String = #function, file: String = #file, line: Int = #line) {
         
         if !condition {
             self.logAndFail(message, level: level, function: function, file: file, line: line)
@@ -50,7 +50,7 @@ public class Logger {
     }
 }
 
-extension Logger {
+public extension Logger {
     enum LogLevel: UInt {
         case critical   = 0
         case error      = 1
